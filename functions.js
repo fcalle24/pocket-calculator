3fb026ea586c8cf68f7c156e88c2284d667eb221
Fabian Calle <facalle24@gmail.com>
2019-01-04 10:49:37 -0500

    added function for AC, 7, & 8

---
+++ functions.js
@@ -0,0 +1,14 @@
+function AC() {
+  let clear = "";
+    document.getElementById("result").innerHTML = clear;
+}
+
+function seven() {
+  let seven = "7";
+  document.getElementById("result").innerHTML += seven;
+}
+
+function eight() {
+  let eight = "8";
+  document.getElementById("result").innerHTML += eight;
+}
